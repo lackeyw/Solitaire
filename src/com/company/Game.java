be2dea20e1634e32c9b214fcdb5ee0d33c04f527
lackeyw <lackeyw15@gmail.com>
2020-07-24 10:54:09 -0400

    Updated smartStack to work with ace.

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -26,11 +26,10 @@ public class Game {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String to;
     String from;
-    String scramble;
 
-    final String avaibleChoices = "1234qwertyu";
+    final String availableChoices = "123456qwertyup";
+    final String aceNums = "1234";
 
-    //TODO need to finish
     public Game() {
 
         //Make sure the play stack is initialized
@@ -94,14 +93,12 @@ public class Game {
         mWon = false;
         from = "";
         to = "";
-        scramble = "b";
 
         generateOutput();
 
         return true;
     }
 
-    //TODO need to remember to remove test cases
     public void runLoop() throws IOException {
        while (mIsRunning)
         {
@@ -109,136 +106,40 @@ public class Game {
             updateGame();
             generateOutput();
         }
-        //testSmartMove();
     }
 
-    private void testSmartMove(){
-        LinkedList<Card> move = new LinkedList<>();
-        LinkedList<Card> stay = new LinkedList<>();
-
-        Card c1 = new Card("Hearts", 4);
-        Card c2 = new Card("Spades", 3);
-        Card c3 = new Card("Diamonds", 2);
-        Card c4 = new Card("Spades", 1);
-
-        Card c5 = new Card("Hearts", 13);
-        Card c6 = new Card("Spades", 12);
-
-        c1.flipCard();
-        c1.reachCard();
-        c2.flipCard();
-        c2.reachCard();
-        c3.flipCard();
-        c3.reachCard();
-        c4.flipCard();
-        c4.reachCard();
-        c5.flipCard();
-        c5.reachCard();
-        c6.flipCard();
-        c6.reachCard();
-
-        move.add(c1);
-        move.add(c2);
-        move.add(c3);
-        move.add(c4);
-
-        stay.add(c5);
-        stay.add(c6);
-
-        for(int i = 0; i < move.size(); i ++){
-            System.out.print(move.get(i).displayCard() + " ");
-        }
-        System.out.println("");
-
-        for(int j = 0; j < stay.size(); j ++){
-            System.out.print(stay.get(j).displayCard() + " ");
-        }
-        System.out.println("");
-
-        smartStack(move, stay);
-
-        move = nesc[0];
-        stay = nesc[1];
-
-        for(int i = 0; i < move.size(); i ++){
-            System.out.print(nesc[0].get(i).displayCard() + " ");
-        }
-        System.out.println("");
-
-        for(int j = 0; j < stay.size(); j ++){
-            System.out.print(nesc[1].get(j).displayCard() + " ");
-        }
-        System.out.println("");
-    }
-
-    //TODO need to add anything here?
     private void processInput() throws IOException {
 
         from = br.readLine();
         if(!from.equals("6") && !from.equals("p")){
             to = br.readLine();
-        }
-
-        if(avaibleChoices.indexOf(to)<0){
-            System.out.println("Scrambled");
-            from = scramble;
+        } else {
+            to = "5";
         }
 
     }
 
-    //TODO all of this, make sure to include a win state, and a game over state;
-    //Tested 6, p, default
     private void updateGame(){
 
-        switch(from){
-            case "6":
+        if(availableChoices.contains(from) && availableChoices.contains(to)){
+            if(aceNums.contains(to)){
+                moveAce(translateKey(from),translateKey(to));
+            }
+            else if(from.equals("6")){
                 drawNewHand();
-                break;
-            case "p":
+            }
+            else if(from.equals("p")){
                 mIsRunning = false;
-                break;
-            case "1":
-                moveAce(aceStack[0], translateKey(to));
-                break;
-            case "2":
-                moveAce(aceStack[1], translateKey(to));
-                break;
-            case "3":
-                moveAce(aceStack[2], translateKey(to));
-                break;
-            case "4":
-                moveAce(aceStack[3], translateKey(to));
-                break;
-            case "5":
-                smartStack(hand, translateKey(to));
-            case "q":
-                smartStack(playStack[0], translateKey(to));
-                break;
-            case "w":
-                smartStack(playStack[1], translateKey(to));
-                break;
-            case "e":
-                smartStack(playStack[2], translateKey(to));
-                break;
-            case "r":
-                smartStack(playStack[3], translateKey(to));
-                break;
-            case "t":
-                smartStack(playStack[4], translateKey(to));
-                break;
-            case "y":
-                smartStack(playStack[5], translateKey(to));
-                break;
-            case "u":
-                smartStack(playStack[6], translateKey(to));
-                break;
-            default:
-                break;
+            }
+            else {
+                smartStack(translateKey(from), translateKey(to));
+            }
         }
-
+        if(aceStack[0].size() == 13 && aceStack[1].size() == 13 && aceStack[2].size() == 13 && aceStack[3].size() == 13){
+            mWon = true;
+        }
     }
 
-    //TODO test when the game has been running for a bit
     //Displays the play table in the console
     private void generateOutput(){
 
@@ -314,12 +215,12 @@ public class Game {
             System.out.print("( t ) ");
             System.out.print("( y ) ");
             System.out.print("( u ) ");
+            System.out.print("\n");
         }
     }
 
     //Draws three new cards into the hand, puts the previous ones on the bottom of the deck
     private void drawNewHand(){
-
         int size;
         Card tempCard = new Card();
 
@@ -334,6 +235,9 @@ public class Game {
                 }
                 playDeck.add(tempCard);
             }
+        }
+        else {
+            System.out.println("hand is empty");
         }
 
         //Draws the top three or however remaining cards, flips them over, lets the last one be reachable
@@ -356,27 +260,35 @@ public class Game {
     }
 
     //Determines what cards to move from the moving stack to the destination
-    //TODO I mean it works but i need to use pointers :(, also need to flip over the remaining card if its flipped
-    //make a case for Ace?
     private void smartStack(LinkedList<Card> move, LinkedList<Card> stay){
-
-        Card finish = stay.getLast();
         Card start;
+        Card finish;
 
-        for(int i = 0; i < move.size(); i ++){
-            start = move.get(i);
-            if(start.faceUp()){
-                if(checkCompatible(start, finish)){
-                    int temp1 = move.size();
-                    int temp2 = move.indexOf(start);
-                    for(int j = 0; j < temp1 - temp2; j++){
-                        stay.add(move.remove(temp2));
-                    }
-                    if(!move.isEmpty()){
-                        if(!move.getLast().faceUp()){
-                            move.getLast().flipCard();
+        if(stay.isEmpty()){
+            finish = new Card("Spades",14);
+        }
+        else{
+            finish = stay.getLast();
+        }
+
+        if(!move.isEmpty()){
+            for(int i = 0; i < move.size(); i ++){
+                start = move.get(i);
+                if(start.faceUp() && start.isReachable()){
+                    if(checkCompatible(start, finish)){
+                        int temp1 = move.size();
+                        int temp2 = move.indexOf(start);
+                        for(int j = 0; j < temp1 - temp2; j++){
+                            stay.add(move.remove(temp2));
                         }
-                        move.getLast().reachCard();
+                        if(!move.isEmpty()){
+                            if(!move.getLast().faceUp()){
+                                move.getLast().flipCard();
+                            }
+                            if(!move.getLast().isReachable()){
+                                move.getLast().reachCard();
+                            }
+                        }
                     }
                 }
             }
@@ -387,39 +299,41 @@ public class Game {
     }
 
     private void moveAce(LinkedList<Card> move, LinkedList<Card> stay){
-        Card finish = stay.getLast();
-        if(move.isEmpty()){
-            move.add(stay.getLast());
-        }
-        else{
+        if(!move.isEmpty()){
             Card start = move.getLast();
-
-            if (checkCompatible(finish, start)) {
+            if(!stay.isEmpty()){
+                Card finish = stay.getLast();
+                if (checkAceComp(start, finish)) {
+                    stay.add(move.removeLast());
+                    if(!move.isEmpty()){
+                        if(!move.getLast().faceUp()){
+                            move.getLast().flipCard();
+                        }
+                        if(!move.getLast().isReachable()){
+                            move.getLast().reachCard();
+                        }
+                    }
+                }
+            } else if(move.getLast().getRank() == 1){
                 stay.add(move.removeLast());
+                if(!move.isEmpty()){
+                    if(!move.getLast().faceUp()){
+                        move.getLast().flipCard();
+                    }
+                    move.getLast().reachCard();
+                }
             }
         }
-
     }
 
-    //If you're moving it to the ace stack, need to swap moving and inPlace !!!!!!!
     private boolean checkCompatible(Card moving, Card inPlace){
-        return (inPlace.getRank() == moving.getRank() + 1 && inPlace.getColor() != moving.getColor());
+        return (inPlace.getRank() == moving.getRank() + 1 && (inPlace.getColor() != moving.getColor() || inPlace.getRank() == 14));
     }
-
-    //TODO Test(?)
-    //Do I even need this?
-    //If I decide to go back to the original way of doing it, like if its too cluttered, then ill need it
-    private int numFlipped(LinkedList<Card> list){
-        int flipped = 0;
-
-        for (Card card : list) {
-            if (!card.faceUp()) {
-                flipped++;
-            }
-        }
-        return flipped;
+    //Specific case for moving to an ace stack
+    private boolean checkAceComp(Card moving, Card inPlace){
+        return (moving.getRank() == inPlace.getRank() + 1 && inPlace.getSuit() == moving.getSuit());
     }
-
+    
     private LinkedList<Card> translateKey(String input){
         LinkedList<Card> value = new LinkedList<>();
         switch(input){
